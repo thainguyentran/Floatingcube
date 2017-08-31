bbf7e2b46952da7f4050f7c8e340786e07774d8f
Tran Thai Nguyen <thainguyentran@icloud.com>
2017-08-31 17:53:18 +0700

    Create pos_config.py

---
+++ epos_foundation/pos_config.py
@@ -0,0 +1,6 @@
+_columns = {
+    'email_session_report': fields.boolean('Enable Email Session Report', help='Enable feature to Email Session report to Manager'),
+}
+_defaults = {
+        'email_session_report': False,
+    }
